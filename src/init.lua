595924b1144184dd609aac3d845704680ebc554b
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2021-04-02 10:25:31 -0400

    Added integrator windup

--- src/init.lua
+++ src/init.lua
@@ -17,60 +17,10 @@
   
 ]]
 
-local ClampVector = require(script.ClampVector)
-
-local function ClampWrapper(val, params)
-  local min = params.Min
-  local max = params.Max
-  if not min or not max then return val end
-  return math.clamp(val, min, max)
-end
-
-local function ClampVectorWrapper(val, params)
-  local max = params.MaxMagnitude
-  if not max then return val end
-  return ClampVector(val, max)
-end
-
 local PIDController = {}
 PIDController.__index = PIDController
 
 PIDController.Tuner = script.Tuner
-
---[[
-  Default constructor for systems with single numbers as their ouput
-
-  Params:
-    kP: proportional constant
-    kI: integral constant
-    kD: derivative constant
-    min: (optional) minimum output
-    max: (optional) maximum output
-]]
-function PIDController.new(kP: number, kI: number, kD: number, min, max): any
-  return PIDController._new(kP, kI, kD,
-                            0, 
-                            ClampWrapper, 
-                            {Min = min, Max = max})
-end
-
---[[
-  Default constructor for systems with Vector3s as their ouput.
-  Keep in mind since magnitudes can only be positive, there is no
-  minimum value.
-
-  Params:
-    kP: proportional constant
-    kI: integral constant
-    kD: derivative constant
-    maxMagnitude: (optional) maximum threshold for ouput
-]]
-function PIDController.newVector3(kP: number, kI: number, kD: number, maxMagnitude): any
-  return PIDController._new(kP, kI, kD, 
-                            Vector3.new(), 
-                            ClampVectorWrapper, 
-                            {MaxMagnitude = maxMagnitude})
-end
 
 --[[
   Private base constructor that can take numerous types
@@ -79,21 +29,22 @@ end
     kP: proportional constant
     kI: integral constant
     kD: derivative constant
-    default: provided by public constructors, used for initialzation of integral and pastErr
-    clamp: the specific clamp function, it gets the output and clampParams as parameters
-    Bounds: either {min, max} or {maxMagnitude}, sent to clamp function
+    min: minimum value
+    max: maximum value
 ]]
-function PIDController._new(kP: number, kI: number, kD: number, default, clamp, bounds)
+function PIDController.new(kP: number, kI: number, kD: number, min: number, max: number)
   return setmetatable({
     Gains = {
       kP = kP,
       kI = kI,
       kD = kD,
     },
-    Bounds = bounds,
-    _integral = default,
-    _pastErr = default,
-    _clamp = clamp,
+    Bounds = {
+      Min = min or -math.huge,
+      Max = max or math.huge,
+    },
+    _integral = 0,
+    _pastErr = 0,
   }, PIDController)
 end
 
@@ -106,12 +57,24 @@ end
 ]]
 function PIDController:Calculate(setPoint: number, processValue: number, deltaTime: number)
   local err = setPoint - processValue
-  self._integral += err * deltaTime
 	local pOut = self.Gains.kP * err
-	local iOut = self.Gains.kI * self._integral
+	local iOut = 0
+  if not self.IntegratorOff then
+    self._integral += err * deltaTime
+    iOut = self.Gains.kI * self._integral
+  end
 	local dOut = self.Gains.kD * (err - self._pastErr) / deltaTime
+	local rawOutput = pOut + iOut + dOut
+  local output = math.clamp(rawOutput, self.Bounds.Min, self.Bounds.Max)
+
+  self.IntegratorOff = self:_shouldClamp(rawOutput, output, err)
+
 	self._pastErr = err
-	return self._clamp(pOut + iOut + dOut, self.Bounds)
+  return output
+end
+
+function PIDController:_shouldClamp(before, after, err)
+  return before ~= after and math.sign(before) == math.sign(err)
 end
 
 return PIDController
